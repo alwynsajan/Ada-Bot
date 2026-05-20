aa29364760ba012350a2658eb7f86081c43a238d
alwynsajan <alwynsajan@gmail.com>
2026-05-20 13:00:32 +0800

    Added code to get Groq api key and model.

--- config.py
+++ config.py
@@ -5,3 +5,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+GROQ_MODEL = os.getenv("GROQ_MODEL", "llama-3.3-70b-versatile")
